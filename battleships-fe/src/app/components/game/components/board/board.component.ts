24a3bdc4782c3fd2540cbb63fb909e4316cebb60
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-10-10 19:24:21 +0300

    done

--- battleships-fe/src/app/components/game/components/board/board.component.ts
+++ battleships-fe/src/app/components/game/components/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Board, Cell, CellType } from '../../../../shared/models';
+import { BattleshipService } from '../../../../services/battleship.service';
+import { Board, Cell } from '../../../../shared/models';
 
 @Component({
   selector: 'app-board',
@@ -9,82 +10,18 @@ import { Board, Cell, CellType } from '../../../../shared/models';
 export class BoardComponent implements OnInit {
   @Input() board?: Board;
 
-  constructor() {}
+  constructor(private readonly battleshipService: BattleshipService) {}
 
   ngOnInit(): void {}
 
   onCellClick(cell: Cell) {
-    if (cell.type == CellType.Ship) {
-      this.destroyShip(cell);
-    }
+    if (!this.battleshipService.isShipsPlaced) {
+      this.battleshipService.placeShip(cell);
+      this.battleshipService.updatePlayerMap(cell);
+      this.battleshipService.updateShips(cell);
+      const isValid = this.battleshipService.validateShips();
 
-    if (cell.type == CellType.NotShot) {
-      cell.type = CellType.EmptyShot;
-    }
-  }
-
-  destroyShip(cell: Cell) {
-    cell.type = CellType.DamagedShip;
-    const ship = this.getShipCells(cell);
-
-    if (this.isShipDestroyed(ship)) {
-      this.changeAllCellsTypeTo(ship, CellType.DestroyedShip);
-    }
-  }
-
-  isShipDestroyed(ship: Cell[]) {
-    return ship.every((cell) => cell.type === CellType.DamagedShip);
-  }
-
-  changeAllCellsTypeTo(cells: Cell[], type: CellType) {
-    cells.forEach((cell) => (cell.type = type));
-  }
-
-  getShipCells(cell: Cell, ship: Cell[] = []) {
-    const cells: Cell[] = ship;
-    // console.log(cell);
-    const directions = [
-      { x: -1, y: 0 },
-      { x: 1, y: 0 },
-      { y: -1, x: 0 },
-      { y: 1, x: 0 },
-    ];
-
-    directions.forEach((direction) => {
-      const pos = [cell.x + direction.x, cell.y + direction.y];
-      if (!pos.every((pos) => pos > -1 && pos < (this.board?.size || 10)))
-        return;
-      const newCell = this.board?.cells[pos[0]][pos[1]];
-
-      if (newCell === undefined) return;
-      if (cells.includes(newCell)) return;
-      if (
-        ![CellType.Ship, CellType.DamagedShip].some(
-          (type) => type === newCell?.type
-        )
-      )
-        return;
-      cells.push(newCell);
-      cells.concat(this.getShipCells(newCell, cells));
-    });
-
-    return cells;
-  }
-
-  getCellType(cell: Cell) {
-    switch (cell.type) {
-      case CellType.Empty:
-        return 'empty';
-      case CellType.EmptyShot:
-        return 'emptyshot';
-      case CellType.DamagedShip:
-        return 'damage';
-      case CellType.DestroyedShip:
-        return 'destroy';
-      case CellType.NotShot:
-        return 'notshot';
-      case CellType.Ship:
-        return 'ship';
+      this.battleshipService.convertShips();
     }
   }
 }
